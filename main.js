879aabe63c9c6402ac91163f76f2a8ff9bfe16b7
Formadcol <wguevarac85@gmail.com>
2022-11-24 17:22:44 -0500

    Funcion_por_declaracion

---
+++ main.js
@@ -0,0 +1,12 @@
+addEventListener(`DOMContentLoaded`, (e)=>{
+//Funciones por declaracion, Esta funcion perimite declara una funcion que exite a lo largo de todo el codio, se puede ejecutar en cualquier lugar del programa.
+
+function declaracionMasUsada() {
+  return " hola, esta es la forma de funcion mas usada en Javascript"
+}
+
+//Incovar la funcion
+declaracionMasUsada();
+//Mostrar consola
+console.log(declaracionMasUsada());
+})
